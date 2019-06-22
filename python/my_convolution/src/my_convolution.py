9d5ad77bbd06bb35c7bdff5cd14b71cd9ad84193
Kazuhiro Serizawa <nserihiro@gmail.com>
2019-06-22 13:11:31 +0900

    support stride

--- python/my_convolution/src/my_convolution.py
+++ python/my_convolution/src/my_convolution.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 
 
@@ -7,20 +9,21 @@ def convolution_with_numpy(x: np.ndarray, W: np.ndarray, stride: int = 1, pad: i
     n, c_i, h_i, w_i = x.shape
     c_o, c_i, h_k, w_k = W.shape
 
-    h_o = h_i - h_k + 1 + 2 * pad
-    w_o = w_i - w_k + 1 + 2 * pad
+    h_o = math.floor((h_i - h_k + 2 * pad) / float(stride)) + 1
+    w_o = math.floor((w_i - w_k + 2 * pad) / float(stride)) + 1
 
     result = np.zeros((n, c_o, h_o, w_o), dtype=np.float32)
 
     for nn in range(n):
         for cc_i in range(c_i):
             for cc_o in range(c_o):
-                for h in range(h_i - h_k + 1):
-                    for w in range(w_i - w_k + 1):
+                for h in range(h_o):
+                    for w in range(w_o):
                         for k_h in range(h_k):
                             for k_w in range(w_k):
+
                                 result[nn, cc_i * c_i + cc_o, h, w] += \
-                                    x[nn][cc_i][h + k_h][w + k_w] \
+                                    x[nn][cc_i][h * stride + k_h][w * stride + k_w] \
                                     * W[nn][cc_o][k_h][k_w]
 
     return result
--- python/my_convolution/tests/test_my_convolusion.py
+++ python/my_convolution/tests/test_my_convolusion.py
@@ -19,3 +19,35 @@ class TestConvolusionWithNumpy(object):
         actual = convolution_with_numpy(x, W)
 
         assert np.all(np.equal(expected, actual)) == True  # NOQA
+
+    def test_with_stride_2(self):
+        n = 1
+        c_i = 1
+        h_i = 3
+        w_i = 3
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        expected = convolution_2d(x, W, stride=2).data
+        actual = convolution_with_numpy(x, W, stride=2)
+
+        assert np.all(np.equal(expected, actual)) == True  # NOQA
+
+    def test_with_stride_3(self):
+        n = 1
+        c_i = 1
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        expected = convolution_2d(x, W, stride=3).data
+        actual = convolution_with_numpy(x, W, stride=3)
+
+        assert np.all(np.equal(expected, actual)) == True  # NOQA
