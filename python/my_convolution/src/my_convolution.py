7fd5eeca707f84f24b251bec8454670bb2a45ffa
Kazuhiro Serizawa <nserihiro@gmail.com>
2019-06-22 15:21:06 +0900

    support multiproce n, c_i and c_o

--- python/my_convolution/src/my_convolution.py
+++ python/my_convolution/src/my_convolution.py
@@ -9,8 +9,21 @@ def convolution_with_numpy(x: np.ndarray, W: np.ndarray, stride: int = 1, pad: i
     n, c_i, h_i, w_i = x.shape
     c_o, c_i, h_k, w_k = W.shape
 
+    if h_k > h_i or w_k > w_i:
+        raise AssertionError('The height and width of x must be smaller than W')
+
+    if stride > (h_i - h_k + 2 * pad + 1) or stride > (w_i - w_k + 2 * pad + 1):
+        raise AssertionError('The value of stride must be smaller than output tensor size')
+
     h_o = math.floor((h_i - h_k + 2 * pad) / float(stride)) + 1
     w_o = math.floor((w_i - w_k + 2 * pad) / float(stride)) + 1
+
+    if pad > 0:
+        new_x = np.zeros((n, c_i, h_i + 2 * pad, w_i + 2 * pad), dtype=np.float32)
+        for nn in range(n):
+            for cc_i in range(c_i):
+                new_x[nn][cc_i] = np.pad(x[nn][cc_i], pad, 'constant')
+        x = new_x
 
     result = np.zeros((n, c_o, h_o, w_o), dtype=np.float32)
 
@@ -21,9 +34,8 @@ def convolution_with_numpy(x: np.ndarray, W: np.ndarray, stride: int = 1, pad: i
                     for w in range(w_o):
                         for k_h in range(h_k):
                             for k_w in range(w_k):
-
-                                result[nn, cc_i * c_i + cc_o, h, w] += \
-                                    x[nn][cc_i][h * stride + k_h][w * stride + k_w] \
-                                    * W[nn][cc_o][k_h][k_w]
+                                result[nn, cc_o, h, w] += \
+                                    x[nn][cc_i][h * stride + k_h][w * stride + k_w] * \
+                                    W[cc_o][cc_i][k_h][k_w]
 
     return result
--- python/my_convolution/tests/test_my_convolution.py
+++ python/my_convolution/tests/test_my_convolution.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 from chainer.functions.connection.convolution_2d import convolution_2d
 from my_convolution.src.my_convolution import convolution_with_numpy
 from numpy.testing import assert_array_equal
@@ -21,6 +22,22 @@ class TestConvolutionWithNumpy:
 
         assert_array_equal(expected, actual)
 
+    def test_basis_with_1_1_kernel(self):
+        n = 1
+        c_i = 1
+        h_i = 3
+        w_i = 3
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 1
+        w_k = 1
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        expected = convolution_2d(x, W).data
+        actual = convolution_with_numpy(x, W)
+
+        assert_array_equal(expected, actual)
+
     def test_with_stride_2(self):
         n = 1
         c_i = 1
@@ -32,8 +49,11 @@ class TestConvolutionWithNumpy:
         w_k = 2
         W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
 
-        expected = convolution_2d(x, W, stride=2).data
-        actual = convolution_with_numpy(x, W, stride=2)
+        stride = 2
+        pad = 0
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
 
         assert_array_equal(expected, actual)
 
@@ -48,7 +68,345 @@ class TestConvolutionWithNumpy:
         w_k = 2
         W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
 
-        expected = convolution_2d(x, W, stride=3).data
-        actual = convolution_with_numpy(x, W, stride=3)
+        stride = 3
+        pad = 0
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
 
         assert_array_equal(expected, actual)
+
+    def test_with_stride_4(self):
+        n = 1
+        c_i = 1
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 4
+        pad = 0
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_pad_1(self):
+        n = 1
+        c_i = 1
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 1
+        pad = 1
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_stride_3_and_pad_1(self):
+        n = 1
+        c_i = 1
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 3
+        pad = 1
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_stride_3_and_pad_2(self):
+        n = 1
+        c_i = 1
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 3
+        pad = 2
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_n_3(self):
+        n = 3
+        c_i = 1
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 1
+        pad = 0
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_n_3_stride_3_and_pad_1(self):
+        n = 3
+        c_i = 1
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 3
+        pad = 1
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_n_3_stride_3_and_pad_2(self):
+        n = 3
+        c_i = 1
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 3
+        pad = 2
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_c_i_2(self):
+        n = 1
+        c_i = 2
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 1
+        pad = 0
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_c_i_3(self):
+        n = 1
+        c_i = 2
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 1
+        pad = 0
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_c_i_3_n_4_stride_2_and_pad_2(self):
+        n = 4
+        c_i = 3
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 2
+        pad = 2
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_c_o_2(self):
+        n = 1
+        c_i = 1
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 2
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 1
+        pad = 0
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_c_o_3(self):
+        n = 1
+        c_i = 1
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 3
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 1
+        pad = 0
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_c_o_3_and_c_i_2(self):
+        n = 1
+        c_i = 2
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 3
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 1
+        pad = 0
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_c_o_3_and_c_i_3(self):
+        n = 1
+        c_i = 3
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 3
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 1
+        pad = 0
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_c_o_3_c_i_3_and_n_3(self):
+        n = 3
+        c_i = 3
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 3
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 1
+        pad = 0
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_with_c_o_3_c_i_3_n_3_stride_3_and_padding_5(self):
+        n = 3
+        c_i = 3
+        h_i = 10
+        w_i = 10
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 3
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        stride = 3
+        pad = 5
+
+        expected = convolution_2d(x, W, stride=stride, pad=pad).data
+        actual = convolution_with_numpy(x, W, stride=stride, pad=pad)
+
+        assert_array_equal(expected, actual)
+
+    def test_assertion_error_with_too_large_kernel(self):
+        n = 1
+        c_i = 1
+        h_i = 3
+        w_i = 3
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 4  # > h_i
+        w_k = 4  # > w_i
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        with pytest.raises(AssertionError):
+            convolution_with_numpy(x, W)
+
+    def test_assertion_error_with_too_large_stride(self):
+        n = 1
+        c_i = 1
+        h_i = 3
+        w_i = 3
+        x = np.arange(n * c_i * h_i * w_i).reshape(n, c_i, h_i, w_i).astype(np.float32)
+        c_o = 1
+        h_k = 2
+        w_k = 2
+        W = np.full(c_o * c_i * h_k * w_k, 2).reshape(c_o, c_i, h_k, w_k).astype(np.float32)
+
+        pad = 1
+        stride = 5  # > h_i - h_k + 2 * pad + 1
+
+        with pytest.raises(AssertionError):
+            convolution_with_numpy(x, W, stride=stride, pad=pad)
